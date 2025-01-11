7873f7113e3675e74959f05d8cb7b483254a0405
lazaralex98 <alex2012ro@gmail.com>
2025-01-12 00:23:10 +0200

    feat: change embedding conf to prepare for ollama

--- modules/embedding_conf.py
+++ modules/embedding_conf.py
@@ -1,19 +1,27 @@
-# TODO: change this to the model you want to use
-CURRENT_MODEL = "text-embedding-3-large"
+from modules.env import EMBEDDING_MODEL
 
 MODEL_CONFIGS = {
     "text-embedding-3-large": {
+        "provider": "openai",
         "max_tokens": 8191,
         "chunk_size": 3000,
         "dimensions": 3072,
     },
     "text-embedding-3-small": {
+        "provider": "openai",
         "max_tokens": 8191,
         "chunk_size": 2000,
         "dimensions": 1536,
     },
-    # ... other models ...
+    "bge-m3": {
+        "provider": "ollama",
+        "max_tokens": 2048,
+        "chunk_size": 1000,
+        "dimensions": 1024,
+    },
 }
 
-# Set of usage by outside code (e.g. embeddings.py, splitter.py)
-ACTIVE_CONFIG = MODEL_CONFIGS[CURRENT_MODEL]
+if EMBEDDING_MODEL not in MODEL_CONFIGS:
+    raise ValueError(f"Model {EMBEDDING_MODEL} not found in MODEL_CONFIGS")
+
+ACTIVE_CONFIG = MODEL_CONFIGS[EMBEDDING_MODEL]
