3649c2db45416bfccf41340ee74eff15603268b8
lazaralex98 <alex2012ro@gmail.com>
2025-01-12 00:22:59 +0200

    feat: change env vars to prepare for ollama

--- modules/env.py
+++ modules/env.py
@@ -17,12 +17,14 @@ def get_optional_env(key: str, default: Optional[str] = None) -> Optional[str]:
     return os.getenv(key, default)
 
 # Required environment variables
-OPENAI_API_KEY = get_required_env("OPENAI_API_KEY")
+EMBEDDING_MODEL = get_required_env("EMBEDDING_MODEL")
 AWS_ACCESS_KEY_ID = get_required_env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = get_required_env("AWS_SECRET_ACCESS_KEY")
 BUCKET_NAME = get_required_env("BUCKET_NAME")
 
 # Optional environment variables
+OPENAI_API_KEY = get_optional_env("OPENAI_API_KEY")
+OLLAMA_URL = get_optional_env("OLLAMA_URL", "http://localhost:11434")
 API_KEY = get_optional_env("API_KEY")
 STORAGE_URL = get_optional_env("STORAGE_URL")
 LOG_LEVEL = get_optional_env("LOG_LEVEL", "DEBUG")
